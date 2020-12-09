a9c07ccdf230d21b1af0e9fbd7f9cfd0d5627aa1
LaurynE2433 <71398814+LaurynE2433@users.noreply.github.com>
2020-12-09 11:47:29 -0500

    Delete lab5tests.ps1

--- powershell/lab5tests.ps1
+++
@@ -1,8 +0,0 @@
-﻿Param ([Parameter(Mandatory=$true,
-                  position=1,
-                  ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true)]$ObjectName)
-
-"Object name was '$ObjectName'"
-$objTypeName = $ObjectName.gettype().name
-"$ObjectName type is $Objtypename object"
