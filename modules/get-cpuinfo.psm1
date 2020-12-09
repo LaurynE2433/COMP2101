372f2cf2df436eb70c2663bec9d32743677b4fe4
LaurynE2433 <71398814+LaurynE2433@users.noreply.github.com>
2020-12-09 12:49:01 -0500

    changed to psm1

---
+++ modules/get-cpuinfo.psm1
@@ -0,0 +1,10 @@
+﻿$model = (get-ciminstance cim_processor).Name
+$manufacturer = (get-ciminstance cim_processor).Manufacturer
+$speed = (get-ciminstance cim_processor).MaxClockSpeed
+$speed_now = (get-ciminstance cim_processor).CurrentClockSpeed
+$cores = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
+write-output ""
+write-output "Your CPU is an $model made by $manufacturer.
+The current speed is $speed_now
+Its max speed is $speed.
+The number of cores is $cores"
---
+++ modules/get-mydisks.psm1
@@ -0,0 +1,6 @@
+﻿function get-mydisks {
+get-wmiobject win32_diskdrive | foreach {
+new-object -typename psobject -property @{Manufacturer=$_.Manufacturer;Model=$_.Model;
+SerialNumber=$_.SerialNumber;FirmwareRevision=$_.FirmwareRevision;Size=($_.size / 1GB).tostring() + " GB"}
+}
+}
