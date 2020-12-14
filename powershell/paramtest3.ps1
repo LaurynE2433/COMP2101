789189b1c39042fed96013ab9a6229349d7b1942
LaurynE2433 <71398814+LaurynE2433@users.noreply.github.com>
2020-12-14 14:46:42 -0500

    Create paramtest3.ps1

---
+++ powershell/paramtest3.ps1
@@ -0,0 +1,21 @@
+﻿param ( [switch]$System, 
+        [switch]$Disks,
+        [switch]$Network
+)
+
+
+
+$System -eq $false -and $Disks -eq $false -and $Network
+    WMI-Objects.ps1
+    ""
+"### NETWORK ###"
+get-ciminstance win32_networkadapterconfiguration |
+where-object IPEnabled -eq True |
+format-table Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder 
+""
+"### DISKS ###"
+(get-wmiobject win32_diskdrive | foreach {
+new-object -typename psobject -property @{Manufacturer=$_.Manufacturer;Model=$_.Model;
+SerialNumber=$_.SerialNumber;FirmwareRevision=$_.FirmwareRevision;Size=($_.size / 1GB).tostring() + " GB"}
+})
+""
